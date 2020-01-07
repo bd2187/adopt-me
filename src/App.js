c80525d773afb2f3eaca03e27bb5aa5826d18883
bd2187 <brandon.dionisio.26@gmail.com>
2020-01-06 21:15:30 -0800

    Initial commit. Create App and Pet components. Render to DOM

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pet = function(props) {
+    const { name, animal, breed } = props;
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed)
+    ]);
+};
+
+// App Component
+const App = function() {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt me!"),
+        React.createElement(Pet, {
+            name: "Dorito",
+            animal: "Cat",
+            breed: "Manx"
+        }),
+        React.createElement(Pet, {
+            name: "Daisy",
+            animal: "Dog",
+            breed: "Pug"
+        }),
+        React.createElement(Pet, {
+            name: "Charlie",
+            animal: "Dog",
+            breed: "Pug"
+        })
+    ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
