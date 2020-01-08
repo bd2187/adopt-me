515431203206bb2b4b455060507a5b1be1b5088f
bd2187 <brandon.dionisio.26@gmail.com>
2020-01-07 19:45:33 -0800

    Utilize jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
-// App Component
+
 const App = function() {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, "Adopt me!"),
-        React.createElement(Pet, {
-            name: "Dorito",
-            animal: "Cat",
-            breed: "Manx"
-        }),
-        React.createElement(Pet, {
-            name: "Daisy",
-            animal: "Dog",
-            breed: "Pug"
-        }),
-        React.createElement(Pet, {
-            name: "Charlie",
-            animal: "Dog",
-            breed: "Pug"
-        })
-    ]);
+    return (
+        <div>
+            <h1>Adopt me!</h1>
+            <Pet name="Dorito" animal="Cat" breed="Manx" />
+            <Pet name="Daisy" animal="Dog" breed="Pug" />
+            <Pet name="Charlie" animal="Dog" breed="Pug" />
+        </div>
+    );
 };
 
 render(React.createElement(App), document.getElementById("root"));
