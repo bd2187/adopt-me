8d096d6be92dffaf7f557617f3347e04264038a8
bd2187 <brandon.dionisio.26@gmail.com>
2020-01-09 21:51:32 -0800

    Add useEffect hook to keep track of animal and update breed

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = function() {
     const [location, setLocation] = useState("Seattle, WA");
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("Animal", "cat", ANIMALS);
-    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+    const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
+    useEffect(
+        function() {
+            setBreeds([]);
+            setBreed("");
+
+            pet.breeds(animal).then(function({ breeds: apiBreeds }) {
+                const breedStrings = apiBreeds.map(function({ name }) {
+                    return name;
+                });
+
+                setBreeds(breedStrings);
+            });
+        },
+        [animal, setBreeds, setBreed]
+    );
     return (
         <div className="search-params">
             <form>
