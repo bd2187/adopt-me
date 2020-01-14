88fdf5bdd7016f5b636e988c18eb456a2035792a
bd2187 <brandon.dionisio.26@gmail.com>
2020-01-13 19:05:51 -0800

    Re-write returning markup

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-const Pet = function({ name, animal, breed }) {
+const Pet = function({ name, animal, breed, media, location, id }) {
+    const hero =
+        media.length > 0 ? media[0].small : "http://placecorgi.com/300/300";
+
     return (
-        <div>
-            <h1>{name}</h1>
-            <h2>{animal}</h2>
-            <h2>{breed}</h2>
-        </div>
+        <a href={`/details/${id}`} className="pet">
+            <div className="image-container">
+                <img src={hero} alt={name} />
+            </div>
+            <div className="info">
+                <h1>{name}</h1>
+                <h2>{`${animal} - ${breed} - ${location}`}</h2>
+            </div>
+        </a>
     );
 };
 
